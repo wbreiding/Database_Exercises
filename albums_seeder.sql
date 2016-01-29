7a6707d7a3dcd5551fb049785bdf09b16a64950a
wbreiding <wdibean@gmail.com>
2016-01-28 22:30:37 -0600

    insert

---
+++ albums_seeder.sql
@@ -0,0 +1,11 @@
+use codeup_test_db;
+
+insert into albums (artist, name, release_date, sales, genre) values
+  ('Michael Jackson', 'Thriller', 1982, 43400000, 'Pop, Rock, R&B'),
+  ('AC/DC', 'Back in Black', 1980, 25900000, 'Hard Rock'),
+  ('Pink Floyd', 'The Dark Side of the Moon', 1973, 22700000, 'Progressive Rock'),
+  ('Whitney Houston', 'The Bodyguard', 1992, 27400000, 'Soundtrack/R&B'),
+  ('Meat Loaf', 'Bat out of Hell', 1977, 20600000, 'Hard Rock'),
+  ('Eagles', 'Their Greatest Hits (1971-1975)', 1976, 32200000, 'Rock'),
+  ('Bee Gees', 'Saturday Night Fever', 1977, 19000000, 'Disco'),
+  ('Fleetwood Mac', 'Rumours', 1977, 27900000, 'Soft Rock');
